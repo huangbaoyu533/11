536eb059c21c30a88165f0355a90aa7c86761527
huangbaoyu533 <109521872@qq.com>
2016-11-20 22:11:16 +0800

    student oop

---
+++ web download/animal.py
@@ -0,0 +1,15 @@
+class Animal(object):
+    def run(self):
+        print('animal is running')
+class Dog(object):
+    def run(self):
+        print('Dog is running')
+class Cat(object):
+    def run(self):
+        print('Cat is running')
+def run_twice(animal) :
+    animal.run()
+    animal.run()
+a=Animal()
+d=Dog()
+c=Cat()
